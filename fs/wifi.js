ddfd7fe969c49c82d85faad004f6eb910b418529
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2020-10-08 17:09:36 -0300

    Fix led wifi connected in esp8266

--- fs/wifi.js
+++ fs/wifi.js
@@ -7,9 +7,12 @@ let pixel = 0;
  * Network search function.
  * @description Pixel blinks on network discover. Stop blinking when connected. 
  * When the connection is true, set the internal timer with the time of the Zone.
- * When esp8266 is with mode AP Pixel set in blue.
+ * When esp8266/32 is in mode AP, set NEO pixel in blue.
  */
 let integratedLED = Cfg.get('board.led0.pin');
+let integratedState = Cfg.get('board.led0.state');
+GPIO.set_mode(integratedLED, GPIO.MODE_OUTPUT);
+
 let netSearch = function () {
   if (board.ap.state === false) {
     timerId = Timer.set(
@@ -19,7 +22,6 @@ let netSearch = function () {
         board.timer.timerState = Cfg.get('board.timer.timerState');
 
         if (MQTT.isConnected() === false) {
-          GPIO.set_mode(integratedLED, GPIO.MODE_OUTPUT);
           GPIO.toggle(integratedLED);
 
           pixel = (pixel + 1) % numPixels;
@@ -34,11 +36,9 @@ let netSearch = function () {
           setRtcTime(JSON.parse(hourNow), JSON.parse(minNow), JSON.parse(secNow));
           
           if (!board.timer.timerState) {
-            GPIO.set_mode(integratedLED, GPIO.MODE_OUTPUT);
-            GPIO.write(integratedLED, 1);
+            GPIO.write(integratedLED, integratedState);
           } else {
-            GPIO.set_mode(integratedLED, GPIO.MODE_OUTPUT);
-            GPIO.write(integratedLED, 1);
+            GPIO.write(integratedLED, integratedState);
             setOnePixel(1, green);
           }
         } 
@@ -52,7 +52,6 @@ let netSearch = function () {
       200,
       Timer.REPEAT,
       function () {
-        GPIO.set_mode(integratedLED, GPIO.MODE_OUTPUT);
         GPIO.toggle(integratedLED);
         setOnePixel(1, blue);
       },
